8542966786cc6a86b9ab97f28255548e41fd0863
tdwork0414 <tdwork0414@gmail.com>
2018-07-16 22:58:54 +1000

    Added command line options to main

--- src/main.cpp
+++ src/main.cpp
@@ -14,141 +14,189 @@ using json = nlohmann::json;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("]");
-  if (found_null != std::string::npos) {
+std::string hasData(std::string s)
+{
+    auto found_null = s.find("null");
+    auto b1 = s.find_first_of("[");
+    auto b2 = s.find_first_of("]");
+
+    if (found_null != std::string::npos)
+    {
+        return "";
+    }
+    else if (b1 != std::string::npos && b2 != std::string::npos)
+    {
+        return s.substr(b1, b2 - b1 + 1);
+    }
+
     return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-  uWS::Hub h;
+    //Command line data options
+    std::string data_option;
 
-  // Create a Kalman Filter instance
-  UKF ukf;
-  //UKF ukf(UKF::LASER_DATA_ONLY);
-  //UKF ukf(UKF::RADAR_DATA_ONLY);
+    if(argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <data_options>\n"
+                  << "Data options:\n"
+                  << "\t-L         :Use laser data only\n"
+                  << "\t-R         :Use radar data only\n"
+                  << "\t-LR or -RL :Use both laser and radar data"
+                  << std::endl;
+        return 1;
+    }
+    else
+    {
+        data_option = argv[1];
 
-  // used to compute the RMSE later
-  Tools tools;
-  vector<VectorXd> estimations;
-  vector<VectorXd> ground_truth;
+        if( (data_option != "-L")  &&
+            (data_option != "-R")  &&
+            (data_option != "-LR") &&
+            (data_option != "-RL") )
+        {
+            std::cout << "Wrong data option: " << data_option
+                      << "! Exit." << std::endl;
+            return 1;
+        }
+    }
 
-  h.onMessage([&ukf,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    uWS::Hub h;
+
+    // Create a Kalman Filter instance
+    // Default option is using Laser and Radar data
+    UKF ukf;
+
+    if(data_option == "-L")
+    {
+        ukf = UKF(UKF::LASER_DATA_ONLY);
+    }
+    else if(data_option == "-R")
+    {
+        ukf = UKF(UKF::RADAR_DATA_ONLY);
+    }
+
+    // used to compute the RMSE later
+    Tools tools;
+    vector<VectorXd> estimations;
+    vector<VectorXd> ground_truth;
+
+    h.onMessage([&ukf,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
 
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
+        auto s = hasData(std::string(data));
+        if (s != "") {
 
-      auto s = hasData(std::string(data));
-      if (s != "") {
+            auto j = json::parse(s);
 
-        auto j = json::parse(s);
+            std::string event = j[0].get<std::string>();
 
-        std::string event = j[0].get<std::string>();
+            if (event == "telemetry") {
+            // j[1] is the data JSON object
 
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
+            string sensor_measurment = j[1]["sensor_measurement"];
 
-          string sensor_measurment = j[1]["sensor_measurement"];
+            MeasurementPackage meas_package;
+            istringstream iss(sensor_measurment);
+            long long timestamp;
 
-          MeasurementPackage meas_package;
-          istringstream iss(sensor_measurment);
-          long long timestamp;
+            // reads first element from the current line
+            string sensor_type;
+            iss >> sensor_type;
 
-          // reads first element from the current line
-          string sensor_type;
-          iss >> sensor_type;
+            if (sensor_type.compare("L") == 0)
+            {
+                meas_package.sensor_type_ = MeasurementPackage::LASER;
+                meas_package.raw_measurements_ = VectorXd(2);
 
-          if (sensor_type.compare("L") == 0) {
-                    meas_package.sensor_type_ = MeasurementPackage::LASER;
-                  meas_package.raw_measurements_ = VectorXd(2);
-                  float px;
-                    float py;
-                  iss >> px;
-                  iss >> py;
-                  meas_package.raw_measurements_ << px, py;
-                  iss >> timestamp;
-                  meas_package.timestamp_ = timestamp;
-          } else if (sensor_type.compare("R") == 0) {
+                float px;
+                float py;
+                iss >> px;
+                iss >> py;
+                meas_package.raw_measurements_ << px, py;
+                iss >> timestamp;
+                meas_package.timestamp_ = timestamp;
+            }
+            else if (sensor_type.compare("R") == 0)
+            {
+                meas_package.sensor_type_ = MeasurementPackage::RADAR;
+                meas_package.raw_measurements_ = VectorXd(3);
+                float ro;
+                float theta;
 
-                    meas_package.sensor_type_ = MeasurementPackage::RADAR;
-                  meas_package.raw_measurements_ = VectorXd(3);
-                  float ro;
-                    float theta;
-                    float ro_dot;
-                  iss >> ro;
-                  iss >> theta;
-                  iss >> ro_dot;
-                  meas_package.raw_measurements_ << ro,theta, ro_dot;
-                  iss >> timestamp;
-                  meas_package.timestamp_ = timestamp;
-          }
-          float x_gt;
-          float y_gt;
-          float vx_gt;
-          float vy_gt;
-          iss >> x_gt;
-          iss >> y_gt;
-          iss >> vx_gt;
-          iss >> vy_gt;
-          VectorXd gt_values(4);
-          gt_values(0) = x_gt;
-          gt_values(1) = y_gt;
-          gt_values(2) = vx_gt;
-          gt_values(3) = vy_gt;
-          ground_truth.push_back(gt_values);
+                float ro_dot;
+                iss >> ro;
+                iss >> theta;
+                iss >> ro_dot;
+                meas_package.raw_measurements_ << ro,theta, ro_dot;
+                iss >> timestamp;
+                meas_package.timestamp_ = timestamp;
+            }
 
-          //Call ProcessMeasurment(meas_package) for Kalman filter
-          ukf.ProcessMeasurement(meas_package);
+            float x_gt;
+            float y_gt;
 
-          //Push the current estimated x,y positon from the Kalman filter's state vector
-          VectorXd ukf_states = ukf.GetStates();
+            float vx_gt;
+            float vy_gt;
+            iss >> x_gt;
+            iss >> y_gt;
+            iss >> vx_gt;
+            iss >> vy_gt;
+            VectorXd gt_values(4);
+            gt_values(0) = x_gt;
+            gt_values(1) = y_gt;
+            gt_values(2) = vx_gt;
+            gt_values(3) = vy_gt;
+            ground_truth.push_back(gt_values);
 
-          double p_x = ukf_states(0);
-          double p_y = ukf_states(1);
-          double v   = ukf_states(2);
-          double yaw = ukf_states(3);
+            //Call ProcessMeasurment(meas_package) for Kalman filter
+            ukf.ProcessMeasurement(meas_package);
 
-          double v1  = cos(yaw)*v;
-          double v2  = sin(yaw)*v;
+            //Push the current estimated x,y positon from the Kalman filter's state vector
 
-          VectorXd estimate(4);
+            VectorXd ukf_states = ukf.GetStates();
 
-          estimate(0) = p_x;
-          estimate(1) = p_y;
-          estimate(2) = v1;
-          estimate(3) = v2;
-          estimations.push_back(estimate);
+            double p_x = ukf_states(0);
+            double p_y = ukf_states(1);
+            double v   = ukf_states(2);
+            double yaw = ukf_states(3);
 
-          VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+            double v1  = cos(yaw)*v;
+            double v2  = sin(yaw)*v;
 
-          json msgJson;
-          msgJson["estimate_x"] = p_x;
-          msgJson["estimate_y"] = p_y;
-          msgJson["rmse_x"] =  RMSE(0);
-          msgJson["rmse_y"] =  RMSE(1);
-          msgJson["rmse_vx"] = RMSE(2);
-          msgJson["rmse_vy"] = RMSE(3);
-          auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-          // std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            VectorXd estimate(4);
 
+            estimate(0) = p_x;
+            estimate(1) = p_y;
+            estimate(2) = v1;
+            estimate(3) = v2;
+            estimations.push_back(estimate);
+
+            VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+
+            json msgJson;
+            msgJson["estimate_x"] = p_x;
+            msgJson["estimate_y"] = p_y;
+            msgJson["rmse_x"] =  RMSE(0);
+            msgJson["rmse_y"] =  RMSE(1);
+            msgJson["rmse_vx"] = RMSE(2);
+            msgJson["rmse_vy"] = RMSE(3);
+            auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
+            // std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
+            }
         }
-      } else {
-
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
+        else
+        {
+            std::string msg = "42[\"manual\",{}]";
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
     }
 
   });
